95f78d8de6ed859da9013f41e234bafa7f2557cf
Casey Cole <caseycole589@gmail.com>
2018-11-24 16:33:55 -0500

    styled the header

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,28 +2,60 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { setAuthedUser } from '../actions/authUser';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
+const styles = {
+	root: {
+		flexGrow: 1
+	},
+	grow: {
+		marginRight: 20
+	}
+};
 class Main extends Component {
 	logOut = () => {
 		console.log('logging out');
 		this.props.dispatch(setAuthedUser(null));
 	};
+
 	render() {
+		const { classes } = this.props;
 		return (
-			<div className="flex row space-around">
-				<NavLink to="/" exact>
-					Home
-				</NavLink>
-				<NavLink to="/add" exact>
-					New Question
-				</NavLink>
-				<div> Leader Board</div>
-				<div>
-					<button onClick={this.logOut}>Log Out</button>
-					<span style={{ marginLeft: '20px' }}>
-						{this.props.authUser}
-					</span>
-				</div>
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Toolbar>
+						<NavLink to="/" exact>
+							<Typography
+								variant="h6"
+								color="inherit"
+								className={classes.grow}
+							>
+								Home
+							</Typography>
+						</NavLink>
+						<NavLink to="/add" exact>
+							<Typography
+								variant="h6"
+								color="inherit"
+								className={classes.grow}
+							>
+								New Question
+							</Typography>
+						</NavLink>
+						<div> Leader Board</div>
+						<div>
+							<button onClick={this.logOut}>Log Out</button>
+							<span style={{ marginLeft: '20px' }}>
+								{this.props.authUser}
+							</span>
+						</div>
+					</Toolbar>
+				</AppBar>
 			</div>
 		);
 	}
@@ -34,4 +66,4 @@ function mapStateToProps({ authUser }) {
 	};
 }
 
-export default connect(mapStateToProps)(Main);
+export default connect(mapStateToProps)(withStyles(styles)(Main));
